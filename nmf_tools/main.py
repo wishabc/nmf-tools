b8fabd83793d3edeedbff53ed13d75686cab7eb2
wishabc <aswq22013@gmail.com>
2024-08-27 17:15:32 -0700

    Initial commit

---
+++ nmf_tools/main.py
@@ -0,0 +1,12 @@
+from nmf_tools import __version__
+
+
+def print_help():
+    lines = ['-h Print help information.']
+    print('\n'.join(lines))
+
+def main():
+    print_help()
+    print(f'nmf-tools version: {__version__}')
+
+
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+import os
+import re
+
+
+def read_version():
+    path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'src/__init__.py')
+    with open(path, 'r') as fh:
+        return re.search(r'__version__\s?=\s?[\'"](.+)[\'"]', fh.read()).group(1)
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='nmf-tools',
+    version=read_version(),
+    description='',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    author='Alexandr Boytsov',
+    author_email='sboytsov@altius.org',
+    url='https://github.com/wishabc/nmt-tools.git',
+    license='GPL3',
+    packages=['nmf-tools'],
+    entry_points={
+        'console_scripts': [
+           # 'nmf-tools = nmf-tools.__main__:main'
+        ]
+    },
+    install_requires=['pandas', 'numpy', 'matplotlib>=3.3.3',
+                    'scikit-learn', 'seaborn', 'statsmodels',
+                    'jupyterlab'],
+    python_requires='>=3.8',
+    data_files=[("", ["LICENSE"])]
+)
