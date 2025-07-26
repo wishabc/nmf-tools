c837173b93b2001987be4f22e8bd9279d66005b6
wishabc <aswq22013@gmail.com>
2025-07-26 12:39:13 +0700

    fixed check_w_h

--- nmf_tools/nmf/weighted_NMF.py
+++ nmf_tools/nmf/weighted_NMF.py
@@ -1,7 +1,7 @@
 import time
 import numpy as np
 from sklearn.decomposition import NMF
-from sklearn.decomposition._nmf import _beta_loss_to_float, trace_dot
+from sklearn.decomposition._nmf import _beta_loss_to_float, trace_dot, _check_init, _initialize_nmf
 import numpy as np
 import scipy.sparse as sp
 import time
@@ -41,6 +41,63 @@ class WeightedNMF(NMF):
         self.n_iter_ = n_iter
 
         return W
+    
+    def _check_w_h(self, X, W, H, update_H):
+        """Check W and H, or initialize them."""
+        n_samples, n_features = X.shape
+
+        if self.init == "custom" and update_H:
+            _check_init(H, (self._n_components, n_features), "NMF (input H)")
+            _check_init(W, (n_samples, self._n_components), "NMF (input W)")
+            if self._n_components == "auto":
+                self._n_components = H.shape[0]
+
+            if H.dtype != X.dtype or W.dtype != X.dtype:
+                raise TypeError(
+                    "H and W should have the same dtype as X. Got "
+                    "H.dtype = {} and W.dtype = {}.".format(H.dtype, W.dtype)
+                )
+
+        elif not update_H:
+
+            _check_init(H, (self._n_components, n_features), "NMF (input H)")
+            if self._n_components == "auto":
+                self._n_components = H.shape[0]
+
+            if H.dtype != X.dtype:
+                raise TypeError(
+                    "H should have the same dtype as X. Got H.dtype = {}.".format(
+                        H.dtype
+                    )
+                )
+            
+            # 'mu' solver should not be initialized by zeros
+            if self.solver == "mu":
+                if W is None:
+                    avg = np.sqrt(X.mean() / self._n_components)
+                    W = np.full((n_samples, self._n_components), avg, dtype=X.dtype)
+            else:
+                raise NotImplementedError
+
+        else:
+            if W is not None or H is not None:
+                warnings.warn(
+                    (
+                        "When init!='custom', provided W or H are ignored. Set "
+                        " init='custom' to use them as initialization."
+                    ),
+                    RuntimeWarning,
+                )
+
+            if self._n_components == "auto":
+                self._n_components = X.shape[1]
+
+            W, H = _initialize_nmf(
+                X, self._n_components, init=self.init, random_state=self.random_state
+            )
+
+        return W, H
+
 
     def reconstruction_error(self, X, W, H, W_weights=None, H_weights=None):
         """Calculate the NMF reconstruction error."""
