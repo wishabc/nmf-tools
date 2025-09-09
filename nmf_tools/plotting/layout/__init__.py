7730c7e107c2048971c2bce8c48a48e616e60aea
wishabc <aswq22013@gmail.com>
2025-09-09 07:01:07 +0700

    fix: update keyword argument handling in broadcast_zip to use iterators for BroadcastIter

--- nmf_tools/plotting/layout/__init__.py
+++ nmf_tools/plotting/layout/__init__.py
@@ -79,7 +79,7 @@ def broadcast_zip(*args, **kwargs):
     kw_iterables = {}
     for key, value in kwargs.items():
         if isinstance(value, BroadcastIter):
-            kw_iterables[key] = next(value.data)
+            kw_iterables[key] = iter(value.data)
             lengths.add(value.length)
         else:
             kw_iterables[key] = itertools.repeat(value)
