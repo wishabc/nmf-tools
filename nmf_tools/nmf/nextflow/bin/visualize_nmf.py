fix: correct indexing in reproduced_peaks calculation for accurate plotting

nmf_tools/nmf/nextflow/bin/visualize_nmf.py
@@ -61,10 +61,10 @@ def main(
 
     #Only reproduced DHSs
     print('>=4 peaks supporting a DHS')
-    reproduced_peaks = binary_matrix.sum(axis=0) >= 4
+    reproduced_peaks = binary_matrix[:, downsampled_indices].sum(axis=0) >= 4
     fig, ax = plt.subplots(figsize=(20, 2))
     ax, _, _ = component_barplot(
-        H[:, downsampled_indices][:, reproduced_peaks[downsampled_indices]],
+        H[:, downsampled_indices][:, reproduced_peaks],
         component_data,
         normalize_for_plotting=True,
         order_records_by='primary',